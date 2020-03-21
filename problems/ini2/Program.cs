717dadfc3b3c845335d10645d99c1c98e3644254
helio <helio@debit.com.br>
2020-03-21 01:32:43 -0300

    ini2

---
+++ problems/ini2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ini2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(hypotenuse(3, 5));
+        }
+
+        static int hypotenuse(int a, int b)
+        {
+            //Console.WriteLine(System.Math.Pow(a, 2) + System.Math.Pow(b, 2)); 
+            return a * a + b * b;
+        }
+    }
+}
