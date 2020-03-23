93b0c094fb21ca621a6ed250a204378767799fe1
helio <helio@debit.com.br>
2020-03-22 23:42:20 -0300

    ini6 with write text

---
+++ problems/ini6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ini6
+{
+    class Program
+    {
+        static string ReadFile()
+        {
+            string txt = File.ReadAllText(@"/home/debit/Downloads/rosalind_ini6.txt");
+            return txt;
+        }
+        
+        static void WriteFile(string text)
+        {
+            File.WriteAllText(@"/home/debit/Downloads/rosalind_ini6_resp.txt", text);
+        }
+        
+        static void Main(string[] args)
+        {
+            string s = ReadFile();
+            StringBuilder text = new StringBuilder();
+            
+            foreach (var word in CountWords(s))
+            {
+                text.AppendLine(word.Key + " " + word.Value);
+            }
+            
+            WriteFile(text.ToString());
+        }
+
+        private static Dictionary<string, int> CountWords(string s)
+        {
+            Dictionary<string, int> dicWords = new Dictionary<string, int>();
+
+            var splited = s.Trim().Split(" ");
+            foreach (string word in splited)
+            {
+                if (!dicWords.ContainsKey(word))
+                {
+                    dicWords[word] = splited.Count(w=> w == word);
+                }
+            }
+
+            return dicWords;
+        }
+    }
+}
