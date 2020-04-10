fcc9d91c0b55969e14e37e2804f4a278c5e37af0
helio <helio@debit.com.br>
2020-04-09 23:27:20 -0300

    Counting DNA Nucleotides

---
+++ problems/dna/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace dna
+{
+    class Program
+    {
+        static string ReadFile()
+        {
+            string txt = File.ReadAllText(@"/home/debit/Downloads/rosalind_dna.txt");
+            return txt;
+        }
+        
+        static void WriteFile(string text)
+        {
+            File.WriteAllText(@"/home/debit/Downloads/rosalind_dna_resp.txt", text);
+        }
+        
+        static void Main(string[] args)
+        {
+            string s = ReadFile();
+
+            string resp = s.Count(c => c.Equals('A')).ToString() + ' ' +
+                          s.Count(c => c.Equals('C')).ToString() + ' ' +
+                          s.Count(c => c.Equals('G')).ToString() + ' ' +
+                          s.Count(c => c.Equals('T')).ToString();
+            
+            Console.Write(resp);
+            
+            WriteFile(resp);
+        }
+    }
+}
