9a6dff239f6f7d1387ef9513679e4b533f8b1dd4
helio <helio@debit.com.br>
2020-04-13 23:04:34 -0300

    Transcribing DNA into RNA

---
+++ problems/rna/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace rna
+{
+    class Program
+    {
+        static string ReadFile()
+        {
+            string txt = File.ReadAllText(@"/home/debit/Downloads/rosalind_rna.txt");
+            return txt;
+        }
+        
+        static void WriteFile(string text)
+        {
+            File.WriteAllText(@"/home/debit/Downloads/rosalind_rna_resp.txt", text);
+        }
+        
+        static void Main(string[] args)
+        {
+            string s = ReadFile();
+
+            string resp = s.Replace('T', 'U');
+            
+            Console.Write(resp);
+            
+            WriteFile(resp);
+        }
+    }
+}
